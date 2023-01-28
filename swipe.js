5f7607a280db89ac04d1000f859985be2996d41a
yummyflan <phan.alanna@gmail.com>
2023-01-28 16:30:19 -0500

    added photo on front webpage and color changes

---
+++ swipe.js
@@ -0,0 +1,7 @@
+function match(){
+    alert("You matched! Time to study. We will send you an email with a compatible time, date, and location for you to meet your buddy at!");
+}
+
+function reject(){
+    alert("That's the last of your choices!")
+}
