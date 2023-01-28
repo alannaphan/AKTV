27b0360201816dc231c57040e301d9e0964d444f
tramanhvong <vongta21@my.yorku.ca>
2023-01-28 15:32:44 -0500

    skjksjds

--- AKTVserver.js
+++ AKTVserver.js
@@ -1,11 +1,8 @@
 
 var express = require("express");
 var app = express();
-var entryList = [];
-var bolD = false;
-var italiC = false;
-var underlinE = false;
-const passwordList = [];
+var dayAvailable = [];
+var name;
 //receive request from the client side
 app.post('/post',(req,res) => {
     res.header("Access-Control-Allow-Origin","*");
@@ -16,69 +13,15 @@ app.post('/post',(req,res) => {
     //check if the request action is evaluatePassword
     const currentList = [];
     var jsontext;
-    if (z['action'] == "storePassword") {
-        passwordList.push(z['newPassword']);
-        var jsontext = JSON.stringify({
-            'action':'storePassword',
-            'passwordList':passwordList,
-            'msg':'The password has been stored!'
-        });
-        console.log(jsontext);
-        res.send(jsontext);
-    }
-    else if (z['action'] == 'checkPassword') {
-        var jsontext = JSON.stringify({
-            'action':'checkPassword',
-            'passwordList':passwordList,
-            'msg':'This is the list of passwords stored'
-        });
-        console.log(jsontext);
-        res.send(jsontext);
-    }
-    /*else if (z['action'] == 'openHomepage') {
-        var jsontext = JSON.stringify({
-                'action': 'openHomepage',
-                'msg': 'Open the homepage!'
+    if (z['action'] == "createProfile") {
+            var jsontext = JSON.stringify({
+                'action':'createProfile',
+                'msg':'Profile created'
             });
-            console.log(jsontext);
-            res.send(jsontext);
-        }
-    else if (z['action'] == 'accessNewEntry') {
-        jsontext = JSON.stringify({
-            'action':'useData',
-            'username':username,
-            'msg': 'Integrate this data to current client script'
-        });
-        console.log(jsontext);
-        res.send(jsontext);
-    }*/
-    else if (z['action'] == 'saveContent') {
-        //noOfEntry = noOfEntry + 1;
-        entryList.push(z['content']);
-        bolD = z['bold'];
-        italiC = z['italic'];
-        underlinE = z['underline'];
-        var jsontext = JSON.stringify({
-            'action': 'saveContent',
-            'content': entryList,
-            'bold':bolD,
-            'italic':italiC,
-            'underline':underlinE,
-            'msg': 'Current content has been saved'}); 
         console.log(jsontext);
         res.send(jsontext);
         }
-    else if (z['action'] == 'sendContent') {
-        var jsontext = JSON.stringify({
-            'action': 'sendContent',
-            'content': entryList,
-            'bold':bolD,
-            'italic':italiC,
-            'underline':underlinE,
-            'msg': 'Current content has been saved'}); 
-        console.log(jsontext);
-        res.send(jsontext);
-    }
 }
+  
 ).listen(3000);
 console.log("Server is running!");
--- introForm.js
+++ introForm.js
@@ -6,11 +6,43 @@ function submit() {
     var name = document.getElementById("fname");
     var major = document.getElementById("major");
     const day = [];
-    var input = document.getElementsByClassName("dayAvailable");
-    for (var i=0; input[i]; ++i) {
-        if(input[i].checked) {
-            day.push(input.value);
-        }
+    var input;
+    for (var i=1; i<=5; i++) {
+        switch  (i) {
+            case 1:
+                input = document.getElementById("day1");
+                if(input.checked==true) {
+                    day.push("Monday");
+                }
+                break;
+            case 2:
+                input = document.getElementById("day2");
+                if(input.checked==true) {
+                    day.push("Tuesday");
+                }
+                break;
+            case 3:
+                input = document.getElementById("day3");
+                if(input.checked==true) {
+                    day.push("Wednesday");
+                }
+                break;
+            case 4:
+                input = document.getElementById("day4");
+                if(input.checked==true) {
+                    day.push("Thursday");
+                }
+                break;
+            case 5:
+                input = document.getElementById("day5");
+                if(input.checked==true) {
+                    day.push("Friday");
+                }
+                break;
+            }
+    }
+    if (day.length==0) {
+        alert("Please choose the days you are available");
     }
 
     $.post(
@@ -22,14 +54,14 @@ function submit() {
         'action':'createProfile'
         }),
         response);
-    window.location.href = "swipe.html";
+        window.location.href = "swipe.html";
 }
 
 function response(data,status) {
     var response = JSON.parse(data);
     console.log(data);
-    if (response['action'] == 'insufficientInfo') {
-        alert("Missing information");
+    if (response['action'] == 'createProfile') {
+        alert("Profile created");
     }
 }
 
