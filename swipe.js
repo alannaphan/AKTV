795dcb0585f53f025a4e6185f6a676ce9e0e20cf
tramanhvong <vongta21@my.yorku.ca>
2023-01-28 20:22:09 -0500

    change

--- swipe.js
+++ swipe.js
@@ -1,7 +1,9 @@
 function match(){
     alert("A+ you matched! Time to hit the books!");
+    window.location.href = "welcomePage.html";
 }
 
 function reject(){
     alert("That's the last of your choices!")
+    window.location.href = "welcomePage.html";
 }
